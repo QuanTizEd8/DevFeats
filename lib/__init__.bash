070ddac52c648d12d332b450a388c6087c38ef0a
QuanTized <35783868+QuanTizEd8@users.noreply.github.com>
2026-05-23 21:59:21 +0200

    Create __init__.bash

---
+++ lib/__init__.bash
@@ -0,0 +1,50 @@
+# Bash Library
+
+_LIB_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# shellcheck source=lib/file.sh
+. "$_LIB_DIR/file.sh"
+# shellcheck source=lib/git.sh
+. "$_LIB_DIR/git.sh"
+# shellcheck source=lib/logging.sh
+. "$_LIB_DIR/logging.sh"
+# shellcheck source=lib/os.sh
+. "$_LIB_DIR/os.sh"
+# shellcheck source=lib/str.sh
+. "$_LIB_DIR/str.sh"
+# shellcheck source=lib/ver.sh
+. "$_LIB_DIR/ver.sh"
+# shellcheck source=lib/json.sh
+. "$_LIB_DIR/json.sh"
+# shellcheck source=lib/net.sh
+. "$_LIB_DIR/net.sh"
+# shellcheck source=lib/verify.sh
+. "$_LIB_DIR/verify.sh"
+# shellcheck source=lib/lock.sh
+. "$_LIB_DIR/lock.sh"
+# shellcheck source=lib/users.sh
+. "$_LIB_DIR/users.sh"
+# shellcheck source=lib/proc.sh
+. "$_LIB_DIR/proc.sh"
+# shellcheck source=lib/graph.sh
+. "$_LIB_DIR/graph.sh"
+# shellcheck source=lib/shell.sh
+. "$_LIB_DIR/shell.sh"
+# shellcheck source=lib/install/common.sh
+. "$_LIB_DIR/install/common.sh"
+# shellcheck source=lib/install/jq.sh
+. "$_LIB_DIR/install/jq.sh"
+# shellcheck source=lib/install/yq.sh
+. "$_LIB_DIR/install/yq.sh"
+# shellcheck source=lib/install/oras.sh
+. "$_LIB_DIR/install/oras.sh"
+# shellcheck source=lib/ospkg.sh
+. "$_LIB_DIR/ospkg.sh"
+# shellcheck source=lib/github.sh
+. "$_LIB_DIR/github.sh"
+# shellcheck source=lib/oci.sh
+. "$_LIB_DIR/oci.sh"
+# shellcheck source=lib/uri.sh
+. "$_LIB_DIR/uri.sh"
+
+unset _LIB_DIR
